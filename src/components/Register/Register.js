002c2dda067bd2dc3963461eef65f31c12041072
Aleksandar <sokolow.aleksandar@gmail.com>
2021-03-30 02:17:21 +0300

    Add register submit handler

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,22 @@
+import { registerUser } from "../../services/authServices";
+
 const Register = () => {
+
+    const handleSubmitRegister = (e) => {
+        e.preventDefault()
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+        const rePassword = e.target['re-password'].value;
+        registerUser(username, password)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+
+    }
+
     return (
         <div className="register">
             <h2>Register Form</h2>
-            <form>
+            <form onSubmit={handleSubmitRegister}>
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" id="username" placeholder="Enter username ... " />
                 <label htmlFor="password">Password</label>
