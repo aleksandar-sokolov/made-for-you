c1ef676a5a1a8a29a78f3101f1e231b4ae97f2bd
Aleksandar <sokolow.aleksandar@gmail.com>
2021-03-30 02:23:33 +0300

    Add login handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,26 @@
+import { loginUser } from "../../services/authServices";
+
 const Login = () => {
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        loginUser(username, password)
+            .then(res => {
+                localStorage.setItem('username', res.username);
+                localStorage.setItem('token', res['user-token']);
+                console.log('successful login');
+            })
+            .catch(console.log)
+
+    }
+
     return (
         <div className="login">
             <h2>Login Form</h2>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" id="username" placeholder="Enter username ... " />
                 <label htmlFor="password">Password</label>
