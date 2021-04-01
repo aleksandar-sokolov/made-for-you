551b4d20e0527f41f38b36b8db109b917317983a
Aleksandar <sokolow.aleksandar@gmail.com>
2021-04-01 11:32:31 +0300

    Update CSS for forms

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -5,7 +5,7 @@ const Page404 = () => {
     return (
         <div className="page-not-found">
             <img src="./Page404.png" alt="Page404" />
-            <h2>Page Not Fount</h2>
+            <h2>Page Not Found</h2>
             <p>Return to <Link to="/">Home</Link> Page?</p>
         </div>
     );
