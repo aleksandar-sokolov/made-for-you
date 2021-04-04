1d3ee7531ce299521703b798b283bbd5443f8b17
Aleksandar <sokolow.aleksandar@gmail.com>
2021-04-04 17:59:50 +0300

    Update HomePage.js

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,19 +1,28 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ErrorContext } from "../../contexts/ErrorContext";
 import productServices from '../../services/productServices';
 import Card from '../Cards/Card'
 import './HomePage.css'
-// import { ErrorContext } from "../../contexts/ErrorContext";
+
 
 const HomePage = () => {
 
+    const { displayError } = useContext(ErrorContext);
 
     const [offers, setOffers] = useState();
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
         productServices.getAll()
             .then(data => {
-                console.log(data);
-                setOffers(data)
+                if (data.hasOwnProperty("errorData")) throw new Error(data.message);
+                setOffers(data);
+                setIsPending(false);
+            })
+            .catch(err => {
+                displayError(err.message);
+                setIsPending(false);
             })
     }, [])
 
@@ -27,6 +36,8 @@ const HomePage = () => {
             <h2>Search, sell or buy handmade products</h2>
             <p>Made 4 You is a platform where you can find the perfect handmade gift or add products you make.After registration you can see the contacts of the sellers or add products that other people can see. </p>
 
+            {isPending && <p>Loading...</p>}
+            {!offers && !isPending && <p>No offers available . <Link to="/product/add">Add first ?</Link> </p>}
             {offers?.map(x => {
                 return (
                     <Card key={x.objectId} {...x} />
