33cf39d2f464955a8a3bacd52d6c57ef9ef13102
Aleksandar <sokolow.aleksandar@gmail.com>
2021-04-10 15:37:47 +0300

    Update UserProfile.js

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,17 +1,27 @@
 import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { ErrorContext } from "../../contexts/ErrorContext";
 import productServices from "../../services/productServices";
 import Card from "../Cards/Card";
 
 const UserProfile = () => {
     const [products, setProducts] = useState([]);
+    const [isPending, setIsPending] = useState(true);
     const { username, userId, userToken } = useContext(AuthContext);
+    const { displayError } = useContext(ErrorContext);
 
     useEffect(() => {
+        setIsPending(true);
         productServices.getAllFromUser(userToken, userId)
             .then(data => {
                 if (data.hasOwnProperty("errorData")) throw new Error(data.message);
                 setProducts(data);
+                setIsPending(false)
+            })
+            .catch(err => {
+                setIsPending(false);
+                displayError(err.message);
             })
     }, [])
 
@@ -20,6 +30,9 @@ const UserProfile = () => {
             <h2>Hello, {username}!</h2>
 
             <h4>Here you can find all your products.</h4>
+
+            {isPending && <p>Loading...</p>}
+            {!products.length && !isPending && <div>We still don't have a product from you. Add your first from <Link to="/product/add">here</Link>. </div>}
             {products && products.map(x => <Card key={x.objectId} {...x} />)}
         </div>
     );
